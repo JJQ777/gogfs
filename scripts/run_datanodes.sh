9a8306cac62a1828755408eca5ff0fb1424ce498
JJQ777 <1143041058@qq.com>
2025-11-05 16:08:32 -0500

    fix sh

--- scripts/run_datanodes.sh
+++ scripts/run_datanodes.sh
@@ -2,7 +2,6 @@
 
 BINARY_NAME="go-dfs"
 
-
 for ((i = 1; i <= 10; i++)); do
   gnome-terminal -- bash -c "cd $(pwd) && ./${BINARY_NAME} datanode -port $((8000 + $i)) -location datanode-files; exec bash"
 done
